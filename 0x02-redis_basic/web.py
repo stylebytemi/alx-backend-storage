2aa77929fc030538c15e38008c035305b1a78a7a
Temiloluwa Adewale <98469754+stylebytemi@users.noreply.github.com>
2023-11-29 13:14:46 +0100

    edit web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-""" expiring web cache module """
+""" Expiring web cache module """
 
 import redis
 import requests
 from typing import Callable
 from functools import wraps
 
-redis = redis.Redis()
-
+# Use a different name for the Redis connection
+redis_conn = redis.Redis()
 
 def wrap_requests(fn: Callable) -> Callable:
     """ Decorator wrapper """
@@ -15,16 +15,15 @@ def wrap_requests(fn: Callable) -> Callable:
     @wraps(fn)
     def wrapper(url):
         """ Wrapper for decorator guy """
-        redis.incr(f"count:{url}")
-        cached_response = redis.get(f"cached:{url}")
+        redis_conn.incr(f"count:{url}")
+        cached_response = redis_conn.get(f"cached:{url}")
         if cached_response:
             return cached_response.decode('utf-8')
         result = fn(url)
-        redis.setex(f"cached:{url}", 10, result)
+        redis_conn.setex(f"cached:{url}", 10, result)
         return result
 
     return wrapper
-
 
 @wrap_requests
 def get_page(url: str) -> str:
